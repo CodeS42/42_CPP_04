d2420c64b5f8e7b4e28892f4e54368294632382a
Scode0 <gandumdiagne.serena@gmail.com>
2024-04-26 02:45:33 +0200

    init some classes

--- ex03/headers/AMateria.hpp
+++ ex03/headers/AMateria.hpp
@@ -1,6 +1,17 @@
 #ifndef A_MATERIA_HPP
 # define A_MATERIA_HPP
 
+class AMateria
+{
+    protected:
+
+    public:
+        AMateria(std::string const & type);
+        
+        std::string const & getType() const; //Returns the materia type
+        virtual AMateria* clone() const = 0;
+        virtual void use(ICharacter& target);
+};
 
 
 #endif
